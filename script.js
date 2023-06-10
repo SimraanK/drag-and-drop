331173ff0697c567f6c7ec3f8c7a941bc04f3014
SimraanK <simraankhatib9@gmail.com>
2023-06-11 02:13:33 +0530

    all done

--- script.js
+++ script.js
@@ -1,45 +1,67 @@
-function handleDragOver(event) {
-    event.preventDefault();
-    event.currentTarget.classList.add('dragging-over');
-}
-
-function handleDrop(event) {
-    event.preventDefault();
-    event.currentTarget.classList.remove('dragging-over');
-    const draggedItem = document.querySelector('.item.dragging');
-    event.currentTarget.appendChild(draggedItem);
-    showMessage('Item dropped successfully!');
-}
-
 const items = document.querySelectorAll('.item');
+const targetContainer = document.getElementById('target-container');
+let draggedItem = null;
+
+// Add event listeners for drag events
 items.forEach(item => {
-    item.addEventListener('dragstart', () => {
-        item.classList.add('dragging');
-    });
-    item.addEventListener('dragend', () => {
-        item.classList.remove('dragging');
-    });
+  item.addEventListener('dragstart', dragStart);
+  item.addEventListener('dragend', dragEnd);
 });
 
-function showMessage(message) {
-    const successMessage = document.createElement('p');
-    successMessage.textContent = message;
-    document.body.appendChild(successMessage);
-    setTimeout(() => {
-        successMessage.remove();
-    }, 2000);
+// Function to handle drag start event
+function dragStart(e) {
+  draggedItem = e.target;
+  e.dataTransfer.setData('text/plain', e.target.id);
+  e.target.classList.add('dragging');
 }
 
+// Function to handle drag end event
+function dragEnd(e) {
+  draggedItem = null;
+  e.target.classList.remove('dragging');
+}
+
+// Function to handle drag enter event
+function dragEnter(e) {
+  e.preventDefault();
+  e.target.classList.add('dragover');
+}
+
+// Function to handle drag over event
+function dragOver(e) {
+  e.preventDefault();
+}
+
+// Function to handle drag leave event
+function dragLeave(e) {
+  e.target.classList.remove('dragover');
+}
+
+// Function to handle drop event
+function drop(e) {
+  e.preventDefault();
+  e.target.classList.remove('dragover');
+
+  if (e.target === targetContainer && draggedItem !== null) {
+    targetContainer.appendChild(draggedItem);
+
+    // Add success class to the dropped item
+    draggedItem.classList.add('success');
+    showMessage('Item dropped successfully!');
+  }
+}
+
+// Reset containers and messages
 function resetContainers() {
-    const draggableContainer = document.querySelector('.draggable');
-    const droppableContainer = document.querySelector('.droppable');
-    const items = document.querySelectorAll('.item');
+  items.forEach(item => {
+    item.classList.remove('success');
+    document.querySelector('.container').appendChild(item);
+  });
+  showMessage('');
+}
 
-    items.forEach(item => {
-        draggableContainer.appendChild(item);
-    });
-
-    while (droppableContainer.firstChild) {
-        droppableContainer.removeChild(droppableContainer.firstChild);
-    }
+// Show success message
+function showMessage(message) {
+  const messageContainer = document.getElementById('message');
+  messageContainer.textContent = message;
 }
